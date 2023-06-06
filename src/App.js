78d0f4b7ab9ff0a525fa2729372db28446224bb2
Badr <moslihbadr2@gmail.com>
2023-06-07 00:26:25 +0100

    add ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div className="App">
       <h1>React Saahla gha bdawha hhh</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
-}
+};
 
 export default App;
