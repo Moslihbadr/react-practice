0ac9f9a83d50048a89da8f24ffcb5fa56794f764
Badr <moslihbadr2@gmail.com>
2023-06-07 22:52:09 +0100

    useState hook practice

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
 import ExpenseDate from './ExpenseDate';
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 
-function ExpenseItem(props) {
+const ExpenseItem = props => {
+  const [amount, setAmount] = useState(props.amount);
 
+  const increase = () => {
+    setAmount(amount + 10);
+  }
+  
   return (
     <div className="expenseItem">
       <ExpenseDate date= {props.date} />
       <div className='expenseItem-description'>
         <h2>{props.title}</h2>
-        <div className='expenseItem-amount'>${props.amount}</div>
+        <div className='expenseItem-amount'>${amount}</div>
       </div>
+      <button onClick={increase}>increase amount by $10</button>
     </div>
   );
 }
